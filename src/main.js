e05a859865be501eee847cdcada808a7fb573146
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2020-10-08 00:20:07 +0300

    add idle sheep

--- src/main.js
+++ src/main.js
@@ -1,6 +1,35 @@
 import { Game, AUTO, Scene } from 'Phaser';
 
+import imageSheepIdle from '../assets/sheep-idle.png';
+
 const scene = new Scene('game');
+
+scene.preload = function () {
+  this.load.spritesheet('sheep-idle', imageSheepIdle, {
+    frameWidth: 325,
+    frameHeight: 300,
+    startFrame: 0,
+    endFrame: 3
+  });
+};
+
+scene.create = function () {
+  this.player = this.physics.add.image(325, 300, 'sheep-idle');
+  this.player.setScale(0.2);
+  this.cameras.main.setBackgroundColor('rgba(100, 180, 250, 1)');
+};
+
+scene.update = function (time, delta) {
+  this.playerDelta = (this.playerDelta || 0) + delta;
+  if (this.playerDelta >= 250) {
+    this.playerDelta = 0;
+    this.playerFrame = (this.playerFrame || 0) + 1;
+    if (this.playerFrame > 3) {
+      this.playerFrame = 0;
+    }
+    this.player.setFrame(this.playerFrame);
+  }
+}
 
 new Game({
   type: AUTO,

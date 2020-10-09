2caa9fe019b71995b9112613ba250d73c2c4f5f4
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2020-10-09 22:51:39 +0300

    add animations jump up jump down

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 import { Game, AUTO, Scene } from 'Phaser';
 
-import imageSheepIdle from '../assets/sheep-idle.png';
+import sheepSpritesheet from '../assets/sheep-spritesheet.png';
 import imageGround from '../assets/ground.png';
 
 const scene = new Scene('game');
 
 scene.preload = function () {
-  this.load.spritesheet('ss-sheep-idle', imageSheepIdle, {
-    frameWidth: 325,
-    frameHeight: 300,
+  this.load.spritesheet('sheep-spritesheet', sheepSpritesheet, {
+    frameWidth: 120,
+    frameHeight: 120,
     startFrame: 0,
-    endFrame: 3
+    endFrame: 15
   });
   this.load.image('image-ground', imageGround);
 };
@@ -18,17 +18,29 @@ scene.preload = function () {
 scene.create = function () {
   this.anims.create({
     key: 'sheep-idle',
-    frames: this.anims.generateFrameNumbers('ss-sheep-idle', { frames: [0, 1, 2, 3] }),
+    frames: this.anims.generateFrameNumbers('sheep-spritesheet', { frames: [0, 1, 2, 3] }),
     frameRate: 4,
     repeat: -1
   });
 
-  this.ground = this.physics.add.staticImage(0, 500, 'image-ground');
-  this.ground.setSize(200, 50);
+  this.anims.create({
+    key: 'sheep-jump-up',
+    frames: this.anims.generateFrameNumbers('sheep-spritesheet', { frames: [4, 5, 6, 7, 8] }),
+    frameRate: 8,
+  });
 
-  this.player = this.physics.add.sprite(0, 300, 'ss-sheep-idle');
-  this.player.setScale(0.25);
-  this.player.setSize(300, 200);
+  this.anims.create({
+    key: 'sheep-jump-down',
+    frames: this.anims.generateFrameNumbers('sheep-spritesheet', { frames: [9, 10, 11] }),
+    frameRate: 8
+  });
+
+  this.ground = this.physics.add.staticImage(0, 500, 'image-ground');
+  this.ground.setSize(200, 75);
+
+  this.player = this.physics.add.sprite(0, 300, 'sheep-spritesheet');
+  this.player.setScale(0.5);
+  this.player.setSize(100, 50);
   this.player.play('sheep-idle');
 
   this.physics.add.collider(this.ground, this.player);
@@ -53,7 +65,7 @@ new Game({
   physics: {
     default: 'arcade',
     arcade: {
-      debug: true,
+      //debug: true,
       gravity: { x: 0, y: 500 }
     }
   }

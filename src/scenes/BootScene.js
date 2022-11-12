6ebe414cd5b353cb2e3d841d36e955566cf00c1f
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2022-11-12 17:17:44 +0100

    change level tiles

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -1,7 +1,7 @@
 import Phaser from 'Phaser';
 
-import LEVEL_1_MAP_JSON from '../../assets/map/level-1-map.json';
-import LEVEL_1_TILESET_PNG from '../../assets/images/level-1-tileset.png';
+import FILE_LEVEL_1_MAP from '../../assets/map/level-1-map.json';
+import FILE_LEVEL_1_TILESET from '../../assets/images/level-1-tileset.png';
 
 import RAM_SPRITESHEET_JSON from '../../assets/images/ram-spritesheet.json';
 import RAM_SPRITESHEET_PNG from '../../assets/images/ram-spritesheet.png';
@@ -31,11 +31,11 @@ export default class BootScene extends Phaser.Scene {
   }
 
   loadMaps() {
-    this.load.tilemapTiledJSON('map-level-1', LEVEL_1_MAP_JSON);
+    this.load.tilemapTiledJSON('map-level-1', FILE_LEVEL_1_MAP);
   }
 
   loadImages() {
-    this.load.image('tileset-level-1', LEVEL_1_TILESET_PNG);
+    this.load.image('level-1-tileset', FILE_LEVEL_1_TILESET);
     this.load.image('background-layer-1', backgroundLayer1);
     this.load.image('background-layer-2', backgroundLayer2);
     this.load.image('background-layer-3', backgroundLayer3);

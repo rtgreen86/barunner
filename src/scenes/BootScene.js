b1d78b74e3b14d2c9bfb147e3da55c99edef49c0
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2022-12-03 23:36:54 +0100

    Cleanup Boot Scene

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -1,27 +1,15 @@
 import Phaser from 'Phaser';
 
-import FILE_LEVEL_1_MAP from '../../assets/map/level-1-map.json';
-import FILE_LEVEL_1_TILESET from '../../assets/images/level-1-tileset.png';
-
+import FILE_BACKGROUND_LAYER_1_PNG from '../../assets/images/background-layer-1.png'
+import FILE_BACKGROUND_LAYER_2_PNG from '../../assets/images/background-layer-2.png'
+import FILE_BACKGROUND_LAYER_3_PNG from '../../assets/images/background-layer-3.png'
+import FILE_LEVEL_1_MAP_JSON from '../../assets/map/level-1-map.json';
+import FILE_LEVEL_1_TILESET_PNG from '../../assets/images/level-1-tileset.png';
 import FILE_OBJECTS_JSON from '../../assets/images/objects.json';
 import FILE_OBJECTS_PNG from '../../assets/images/objects.png';
-
-import FILE_BACKGROUN_EXPERIMENT from '../../assets/images/background-experiment.png';
-
-import RAM_SPRITESHEET_JSON from '../../assets/images/ram-spritesheet.json';
-import RAM_SPRITESHEET_PNG from '../../assets/images/ram-spritesheet.png';
-
-
-import OBSTACLE_PNG from '../../assets/images/obstacle.png';
-
-import backgroundLayer1 from '../../assets/images/background-layer-1.png'
-import backgroundLayer2 from '../../assets/images/background-layer-2.png'
-import backgroundLayer3 from '../../assets/images/background-layer-3.png'
-
-import spritesheet from '../../assets/images/spritesheet.png';
-import spritesheet64 from '../../assets/images/spritesheet-64.png';
-
-import jump from '../../assets/sound/jump.wav';
+import FILE_RAM_SPRITESHEET_JSON from '../../assets/images/ram-spritesheet.json';
+import FILE_RAM_SPRITESHEET_PNG from '../../assets/images/ram-spritesheet.png';
+import FILE_JUMP_WAV from '../../assets/sound/jump.wav';
 
 export default class BootScene extends Phaser.Scene {
   constructor() {
@@ -29,52 +17,33 @@ export default class BootScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.aseprite('ram-spritesheet', RAM_SPRITESHEET_PNG, RAM_SPRITESHEET_JSON);
-    this.load.aseprite('objects-spritesheet', FILE_OBJECTS_PNG, FILE_OBJECTS_JSON);
+    this.loadBackgrounds();
     this.loadMaps();
-    this.loadImages();
-    this.loadSpriteSheets();
+    this.loadSprites();
     this.loadSounds();
   }
 
+  loadBackgrounds() {
+    this.load.image('background-layer-1', FILE_BACKGROUND_LAYER_1_PNG);
+    this.load.image('background-layer-2', FILE_BACKGROUND_LAYER_2_PNG);
+    this.load.image('background-layer-3', FILE_BACKGROUND_LAYER_3_PNG);
+  }
+
   loadMaps() {
-    this.load.tilemapTiledJSON('map-level-1', FILE_LEVEL_1_MAP);
+    this.load.tilemapTiledJSON('map-level-1', FILE_LEVEL_1_MAP_JSON);
+    this.load.image('level-1-tileset', FILE_LEVEL_1_TILESET_PNG);
   }
 
-  loadImages() {
-    this.load.image('level-1-tileset', FILE_LEVEL_1_TILESET);
-    this.load.image('background-layer-1', backgroundLayer1);
-    this.load.image('background-layer-2', backgroundLayer2);
-    this.load.image('background-layer-3', backgroundLayer3);
-    this.load.image('background-experiment', FILE_BACKGROUN_EXPERIMENT);
-    this.load.spritesheet('obstacle-png', OBSTACLE_PNG, { frameWidth: 128, frameHeight: 128 });
-  }
-
-  loadSpriteSheets() {
-    this.load.spritesheet('spritesheet-small', spritesheet, {
-      frameWidth: 64,
-      frameHeight: 64,
-      startFrame: 0,
-      endFrame: 59
-    });
-    this.load.spritesheet('spritesheet-large', spritesheet, {
-      frameWidth: 128,
-      frameHeight: 128,
-      startFrame: 15,
-      endFrame: 25
-    });
-    this.load.spritesheet('spritesheet-64', spritesheet64, {
-      frameWidth: 64,
-      frameHeight: 64
-    });
+  loadSprites() {
+    this.load.aseprite('ram-spritesheet', FILE_RAM_SPRITESHEET_PNG, FILE_RAM_SPRITESHEET_JSON);
+    this.load.aseprite('objects-spritesheet', FILE_OBJECTS_PNG, FILE_OBJECTS_JSON);
   }
 
   loadSounds() {
-    this.load.audio('jump', [jump]);
+    this.load.audio('jump', [FILE_JUMP_WAV]);
   }
 
   create() {
-    console.log('boot scene create');
     this.scene.run('GameScene');
     this.scene.run('DebugScene');
   }

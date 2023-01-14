6bde4887687a43288278f49cdf50bebd8b6cc975
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2023-01-14 22:29:28 +0100

    add kilometers

--- src/scenes/ScoreboardScene.js
+++ src/scenes/ScoreboardScene.js
@@ -10,14 +10,25 @@ export default class ScoreboardScene extends Phaser.Scene {
   }
 
   create() {
-    this.distanceText = this.add.text(0, 720 - 64, '', { font: '64px Arial', fill: '#000000' });
+    this.distanceText = this.add.text(0, 0, '', {
+      font: '32px Arial', color: '#ffffff',
+      shadow: { offsetX: 2, offsetY: 2, fill: '#000000' }
+    });
     this.speedText = this.add.text(200,  720 - 64, '', { font: '64px Arial', fill: '#000000' });
     this.scoreText = this.add.text(400,  720 - 64, '', { font: '64px Arial', fill: '#000000' });
     this.livesText = this.add.text(600,  720 - 64, '', { font: '64px Arial', fill: '#000000' });
   }
 
   update() {
-    this.distanceText.setText('D: ' + this.game.data.get('distance'));
+    const distanceInMeters = Math.floor(this.game.data.get('distance'));
+    const distanceInKilometers = Math.floor(distanceInMeters / 10) / 100;
+
+    if (distanceInMeters < 1000) {
+      this.distanceText.setText('Distance: ' + distanceInMeters + ' m');
+    } else {
+      this.distanceText.setText('Distance: ' + distanceInKilometers.toFixed(2) + ' km');
+    }
+
     this.speedText.setText('S: ' + this.game.data.get('speed'));
     this.scoreText.setText('S: ' + this.game.data.get('score'));
     this.livesText.setText('L: ' + this.game.data.get('lives'));

0804bb6dde9a19eb51e109967e9edf2ca7b49c30
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2023-01-22 11:48:49 +0100

    move styles to separate file

---
+++ src/Styles.js
@@ -0,0 +1,8 @@
+export const uiText = {
+  font: '32px Arial', color: '#ffffff',
+  shadow: { offsetX: 2, offsetY: 2, fill: '#000000' }
+};
+
+export const buttonText = {
+  font: '32px Arial', color: '#ffffff',
+};
--- src/scenes/ScoreboardScene.js
+++ src/scenes/ScoreboardScene.js
@@ -1,5 +1,7 @@
 import Phaser from 'Phaser';
 
+import * as Styles from '../Styles';
+
 export default class ScoreboardScene extends Phaser.Scene {
   constructor() {
     super('ScoreboardScene');
@@ -10,27 +12,17 @@ export default class ScoreboardScene extends Phaser.Scene {
   }
 
   create() {
-    this.distanceText = this.add.text(0, 0, '', {
-      font: '32px Arial', color: '#ffffff',
-      shadow: { offsetX: 2, offsetY: 2, fill: '#000000' }
-    });
-    this.beatsText = this.add.text(350, 0, '', {
-      font: '32px Arial', color: '#ffffff',
-      shadow: { offsetX: 2, offsetY: 2, fill: '#000000' }
-    });
+    this.distanceText = this.add.text(0, 0, '', Styles.uiText);
+    this.beatsText = this.add.text(350, 0, '', Styles.uiText);
 
     this.game.events.on('changedata-beats', (gameObject, value) => {
       this.beatsText.setText('Beats: ' + value);
     });
 
     this.button = this.add.image(300, 100, 'button-green').setScrollFactor(0, 0);
-    this.buttonText = this.add.text(300, 100, 'Menu Menu', {
-      font: '32px Arial', color: '#ffffff',
-      // shadow: { offsetX: 2, offsetY: 2, fill: '#000000' }
-    }).setScrollFactor(0, 0).setOrigin(0.5, 0.5);
+    this.buttonText = this.add.text(300, 100, 'Menu Menu', Styles.buttonText).setScrollFactor(0, 0).setOrigin(0.5, 0.5);
 
     this.button = this.add.image(500, 100, 'button-x').setScrollFactor(0, 0);
-    // this.switch = this.add.image(130, 100, 'switch').setScrollFactor(0, 0);
     this.anims.createFromAseprite('switch');
     this.anims.get('blink').repeat = -1;
     this.switch = this.add.sprite(130, 100, 'switch', 0);

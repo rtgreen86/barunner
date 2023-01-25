9198e3d8c2188072b3fa74d7bf2f0b4ba1131a3b
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2023-01-25 22:32:46 +0100

    disable test button

--- src/scenes/ScoreboardScene.js
+++ src/scenes/ScoreboardScene.js
@@ -21,9 +21,6 @@ export default class ScoreboardScene extends Phaser.Scene {
       this.beatsText.setText('Beats: ' + value);
     });
 
-    this.button = this.add.image(300, 100, 'button-green').setScrollFactor(0, 0);
-    this.buttonText = this.add.text(300, 100, 'Menu Menu', Styles.buttonText).setScrollFactor(0, 0).setOrigin(0.5, 0.5);
-
     const width = this.game.game.config.width;
 
     this.button1 = this.add.existing(new Button(this, width - 64, 64, 'button-x'));
@@ -34,10 +31,11 @@ export default class ScoreboardScene extends Phaser.Scene {
       this.scene.sleep('ScoreboardScene');
     })
 
-    this.anims.createFromAseprite('switch');
-    this.anims.get('blink').repeat = -1;
-    this.switch = this.add.sprite(130, 100, 'switch', 0);
-    this.switch.play('blink');
+    // This example of arrow used as cursor for buttons
+    // this.anims.createFromAseprite('switch');
+    // this.anims.get('blink').repeat = -1;
+    // this.switch = this.add.sprite(130, 100, 'switch', 0);
+    // this.switch.play('blink');
   }
 
   update() {
@@ -49,6 +47,4 @@ export default class ScoreboardScene extends Phaser.Scene {
       this.distanceText.setText('Distance: ' + distanceInKilometers.toFixed(2) + ' km');
     }
   }
-
-
 }

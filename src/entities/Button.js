5ba12a27aa0f2166fab662e9c5fe7a0a22796bdc
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2023-01-28 21:55:34 +0100

    refactor button

--- src/entities/Button.js
+++ src/entities/Button.js
@@ -5,17 +5,21 @@ import * as Styles from '../Styles';
 export default class Button extends Phaser.GameObjects.Container {
   constructor(scene, x, y, texture, frame, text = '') {
     super(scene, x, y, [
-      new Phaser.GameObjects.Image(scene, 0, 0, texture, frame).setName('background'),
-      new Phaser.GameObjects.Text(scene, 0, 0, text, Styles.buttonText).setOrigin(0.5, 0.5).setName('text')
+      scene.add.image(0, 0, texture, frame).setName('background'),
+      scene.add.text(0, 0, text, Styles.buttonText).setOrigin(0.5, 0.5).setName('text')
     ]);
+
+    const background = this.getByName('background');
+    const width = background.width;
+    const height = background.height;
+
+    const dbg = this.scene.scene.get('DebugScene');
+    dbg.log(this.width + ' ' + width);
 
     this.isDown = false;
     this.style = {};
 
     this.setScrollFactor(0, 0);
-    const background = this.getByName('background');
-    const width = background.width;
-    const height = background.height;
     this.setInteractive(new Phaser.Geom.Rectangle(-width / 2, -height / 2, width, height), Phaser.Geom.Rectangle.Contains);
 
     this.on(Phaser.Input.Events.POINTER_DOWN, this.handlePointerDown);

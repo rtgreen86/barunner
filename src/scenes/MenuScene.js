5ae100b5d213fd185d65e095a4e9b8131910fa62
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2023-01-31 22:59:20 +0100

    refactor menu scene

--- src/scenes/MenuScene.js
+++ src/scenes/MenuScene.js
@@ -10,84 +10,63 @@ export default class MenuScene extends Phaser.Scene {
   }
 
   create() {
-    const debug = this.scene.get('DebugScene');
-
     this.add.text(640, 100, 'Пауза', Styles.uiText).setScrollFactor(0, 0).setOrigin(0.5, 0.5);
 
-    this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESC, true, false).on('down', () => {
-      this.scene.stop('MenuScene');
-      this.scene.resume('GameScene');
-      this.scene.wake('ScoreboardScene');
-    });
+    this.buttons = [
+      this.add.existing(new Button(this, 640, 200, 'button-gray', 0, 'Продовжити'))
+        .setStyle('greenButton')
+        .setFocus(true)
+        .on('click', this.gotoGame, this),
 
-    this.buttons = [];
+      this.add.existing(new Button(this, 640, 300, 'button-gray', 0, 'Спочатку'))
+        .setStyle('redButton')
+        .on('click', this.resetGame, this)
+    ];
 
-    const btn1 = this.add.existing(new Button(this, 640, 200, 'button-gray', 0, 'Продовжити'))
-      .setStyle('greenButton')
-      .on('click', () => {
+    this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESC, false, false).on('down', this.gotoGame, this);
+    this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP, false, false).on(Phaser.Input.Keyboard.Events.DOWN, this.handleArrowKeyPressed, this);
+    this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN, false, false).on(Phaser.Input.Keyboard.Events.DOWN, this.handleArrowKeyPressed, this);
+    this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER, false, false).on(Phaser.Input.Keyboard.Events.DOWN, this.handleEnterKeyPressed, this);
+  }
 
-        debug.log('clicked');
+  gotoGame() {
+    this.scene.stop('MenuScene');
+    this.scene.resume('GameScene');
+    this.scene.wake('ScoreboardScene');
+  }
 
-        this.scene.stop('MenuScene');
-        this.scene.resume('GameScene');
-        this.scene.wake('ScoreboardScene');
-      })
-      .setFocus(true);
+  resetGame() {
+    const gameScene = this.scene.get('GameScene');
+    gameScene.scene.restart();
+    const ScoreboardScene = this.scene.get('ScoreboardScene');
+    ScoreboardScene.scene.restart();
+    this.scene.stop('MenuScene');
+  }
 
-    const btn2 = this.add.existing(new Button(this, 640, 300, 'button-gray', 0, 'Спочатку'))
-      .setStyle('redButton')
-      .on('click', () => {
-        const gameScene = this.scene.get('GameScene');
-        gameScene.scene.restart();
-        const ScoreboardScene = this.scene.get('ScoreboardScene');
-        ScoreboardScene.scene.restart();
-        this.scene.stop('MenuScene');
-      });
+  handleArrowKeyPressed(event) {
+    let index = this.buttons.findIndex(item => item.isFocus);
+    if (index > -1) {
+      this.buttons[index].isFocus = false
+    }
+    if (event.keyCode === Phaser.Input.Keyboard.KeyCodes.UP) {
+      index--;
+    }
+    if (event.keyCode === Phaser.Input.Keyboard.KeyCodes.DOWN) {
+      index++;
+    }
+    if (index < 0) {
+      index = this.buttons.length - 1;
+    }
+    if (index === this.buttons.length) {
+      index = 0;
+    }
+    this.buttons[index].isFocus = true
+  }
 
-    this.buttons.push(btn1);
-    this.buttons.push(btn2);
-
-
-    this.up = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP, false, false).on(Phaser.Input.Keyboard.Events.DOWN, () => {
-      let activeIndex = this.buttons.findIndex(item => item.isFocus);
-      if (activeIndex > -1) {
-        this.buttons[activeIndex].isFocus = false
-      }
-
-      activeIndex--;
-
-      if (activeIndex < 0) {
-        activeIndex = this.buttons.length - 1;
-      }
-
-      this.buttons[activeIndex].isFocus = true
-    });
-
-    this.down = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN, false, false).on(Phaser.Input.Keyboard.Events.DOWN, () => {
-      let activeIndex = this.buttons.findIndex(item => item.isFocus);
-      if (activeIndex > -1) {
-        this.buttons[activeIndex].isFocus = false
-      }
-
-      activeIndex++;
-
-      if (activeIndex === this.buttons.length) {
-        activeIndex = 0;
-      }
-
-      this.buttons[activeIndex].isFocus = true
-    });
-
-    this.Enter = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER, false, false).on(Phaser.Input.Keyboard.Events.DOWN, () => {
-      let activeIndex = this.buttons.findIndex(item => item.isFocus);
-
-      if (activeIndex > -1) {
-        debug.log('enter' + activeIndex);
-        const r = this.buttons[activeIndex].emit('click');
-        debug.log(r);
-      }
-
-
-    });
+  handleEnterKeyPressed() {
+    const activeIndex = this.buttons.findIndex(item => item.isFocus);
+    if (activeIndex > -1) {
+      this.buttons[activeIndex].emit('click');
+    }
   }
 }

aa0d042f95531abd4b357781d39e52ba786a11d3
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2023-11-16 22:56:03 +0100

    Move menu cursor by keyboard

--- src/plugins/BaUI/GameObjects/Menu.js
+++ src/plugins/BaUI/GameObjects/Menu.js
@@ -1,17 +1,35 @@
 import Phaser from 'phaser';
 
+export const DIRECTION_UP = 'up';
+
+export const DIRECTION_DOWN = 'down';
+
 export default class Menu extends Phaser.GameObjects.GameObject {
-  #controls = new Set();
+  static DIRECTION_UP = DIRECTION_UP;
+
+  static DIRECTION_DOWN = DIRECTION_DOWN;
+
+  #controls = [];
 
   #marker;
 
+  #active;
+
+  #upKey;
+
+  #downKey;
+
   constructor(scene, texture, frameOrAnimationName, animationName) {
     super(scene, 'menu');
+
     this.#marker = scene.add.baMarker(0, 0, texture, frameOrAnimationName, animationName);
+
+    this.#upKey = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP, true, false).on('down', this.#handleUpPressed, this);
+    this.#upKey = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN, true, false).on('down', this.#handleDownPressed, this);
   }
 
   get length() {
-    return this.#controls.size;
+    return this.#controls.length;
   }
 
   set length(value) {
@@ -19,17 +37,37 @@ export default class Menu extends Phaser.GameObjects.GameObject {
   }
 
   add(gameObject) {
-    this.#controls.add(gameObject);
+    this.#controls.push(gameObject);
+    return this;
+  }
+
+  destroy() {
+    this.#upKey.off('down', this.#handleUpPressed, this);
+    this.#upKey.off('down', this.#handleDownPressed, this);
+  }
+
+  moveMarker(direction) {
+    let index = this.#controls.indexOf(this.#active);
+
+    if (direction === DIRECTION_UP) index--;
+    if (direction === DIRECTION_DOWN) index++;
+    if (index < 0) index = this.length - 1;
+    if (index === this.length) index = 0;
+
+    this.setActive(this.#controls[index]);
+
     return this;
   }
 
   remove(gameObject) {
-    this.#controls.remove(gameObject);
+    const index = this.#controls.indexOf(gameObject);
+    if (index > -1) this.#controls.splice(index, 1);
     return this;
   }
 
   setActive(gameObject) {
     this.#marker.setPosition(gameObject);
+    this.#active = gameObject;
     return this;
   }
 
@@ -41,5 +79,13 @@ export default class Menu extends Phaser.GameObjects.GameObject {
   setOrigin(x, y) {
     this.#marker.setOrigin(x, y);
     return this;
+  }
+
+  #handleUpPressed() {
+    this.moveMarker(DIRECTION_UP);
+  }
+
+  #handleDownPressed() {
+    this.moveMarker(DIRECTION_DOWN);
   }
 }
